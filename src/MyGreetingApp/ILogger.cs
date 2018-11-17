df0badda46bc7999f439d877152d6c33a761d157
Carlos Schults <carlos.schults@gmail.com>
2018-11-17 14:17:30 -0200

    add the logger interface

---
+++ src/MyGreetingApp/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGreetingApp
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
