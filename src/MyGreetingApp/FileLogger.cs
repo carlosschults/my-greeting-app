58546a2510c8e25e28b7320de6c1e2a514ca5397
Carlos Schults <carlos.schults@gmail.com>
2018-11-17 17:08:55 -0200

    create the file logger class

---
+++ src/MyGreetingApp/FileLogger.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using static log4net.LogManager;
+using System.Reflection;
+
+namespace MyGreetingApp
+{
+    public class FileLogger : ILogger
+    {
+        private static readonly ILog log = GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public void Info(string message)
+        {
+            log.Info(message); ;
+        }
+
+        public void Warning(string message)
+        {
+            log.Warn(message);
+        }
+    }
+}
