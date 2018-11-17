fe58503828bc29baa7a0069ffc7502ca4e94896f
Carlos Schults <carlos.schults@gmail.com>
2018-11-17 13:54:47 -0200

    add implemention of the clock interface

---
+++ src/MyGreetingApp/SystemClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MyGreetingApp.Business;
+
+namespace MyGreetingApp
+{
+    public class SystemClock : IClock
+    {
+        public DateTimeOffset Now() => DateTimeOffset.Now;
+    }
+}
